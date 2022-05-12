92de0c8a3b34ef39a9544ced2ec25c5e09206403
6564747347456845 <webkonditer@yandex.ru>
2022-05-12 20:24:28 +0400

    Исправлено форматирование

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,10 @@ public class Main {
         Autor autor2 = new Autor("Сергей", "Есенин");
 
         Book book1 = new Book("Бесы", autor1, "1736");
-        Book book2 = new Book("Шагане ты моя Шагане", autor2, "1736");
+        Book book2 = new Book("Шагане ты моя Шагане", autor2, "1924");
 
         book1.setBookDate("2022");
 
-            System.out.println(book1.getAutor().getAutorName() + " " + book1.getAutor().getAutorSirname() + " " +
-            book1.getBookTitle() + " " + book1.getBookDate());
+        System.out.println(book1.getAutor().getAutorName() + " " + book1.getAutor().getAutorSirname() + " " + book1.getBookTitle() + " " + book1.getBookDate());
     }
 }
