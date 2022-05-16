a0cd5869014b1d0dca0462d349f93189e6235609
Vilas <webkonditer@yandex.ru>
2022-05-16 20:04:00 +0300

    Средний уровень

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,37 @@ public class Main {
 
         book1.setBookDate("2022");
 
-        System.out.println(book1.getAutor().getAutorName() + " " + book1.getAutor().getAutorSirname() + " " + book1.getBookTitle() + " " + book1.getBookDate());
+        //System.out.println(book1.getAutor().getAutorName() + " " + book1.getAutor().getAutorSirname() + " " + book1.getBookTitle() + " " + book1.getBookDate());
+
+        //Средний уровень
+
+        Book[] books = new Book[5];
+
+        books[0] = book1;
+        books[1] = book2;
+
+        Autor autor3 = new Autor("Анджей", "Сапковский");
+        Book book3 = new Book("Ведьмак", autor3, "1986");
+
+        addBook(books, book3);
+
+        printBooks(books);
+    }
+
+    public static void addBook(Book[] booksArray, Book newBook){
+        for (int i = 1; i < 5 ; i++) {
+            if (booksArray[i] == null) {
+                booksArray[i] = newBook;
+                break;
+            }
+        }
+    }
+
+    public static void printBooks(Book[] booksArray){
+        for (Book book: booksArray) {
+            if (book != null) {
+                System.out.println(book.getAutor().getAutorName() + " " + book.getAutor().getAutorSirname() + ": " + book.getBookTitle() + ": " + book.getBookDate());
+            }
+        }
     }
 }
